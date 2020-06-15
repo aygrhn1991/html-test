ef6fdb48f18de721835423183bdf2921f4dc36f9
Chenyufeng <411610370@qq.com>
2020-06-15 11:39:41 +0800

    1

--- page/layui.admin/index.js
+++ page/layui.admin/index.js
@@ -26,10 +26,6 @@ app.run(function($rootScope, $location, $timeout) {
         $rootScope.menu = [
             { id: 1, name: '栏目一', select: false, pages: [{ id: 1, name: 'page1', select: false, path: '#/page1' }, { id: 2, name: 'page2', select: false, path: '#/page2' }] },
             { id: 2, name: '栏目二', select: false, pages: [{ id: 3, name: 'page3', select: false, path: '#/page3' }, { id: 4, name: 'page4', select: false, path: '#/page4' }] },
-            { id: 3, name: '栏目三', select: false, pages: [{ id: 3, name: 'page3', select: false, path: '#/page3' }, { id: 4, name: 'page4', select: false, path: '#/page4' }] },
-            { id: 4, name: '栏目四', select: false, pages: [{ id: 3, name: 'page3', select: false, path: '#/page3' }, { id: 4, name: 'page4', select: false, path: '#/page4' }] },
-            { id: 5, name: '栏目五', select: false, pages: [{ id: 3, name: 'page3', select: false, path: '#/page3' }, { id: 4, name: 'page4', select: false, path: '#/page4' }] },
-            { id: 6, name: '栏目六', select: false, pages: [{ id: 3, name: 'page3', select: false, path: '#/page3' }, { id: 4, name: 'page4', select: false, path: '#/page4' }] },
         ];
         layui.use('element', function() {
             var element = layui.element;
@@ -60,22 +56,7 @@ app.run(function($rootScope, $location, $timeout) {
         e.select = true;
     };
 });
-app.controller('page1Ctrl', function($scope, $http, $rootScope) {
-    $scope.openModal = function() {
-        layer.open({
-            type: 1,
-            content: $('#modal'),
-            shade: 0
-        });
-    };
-    $scope.model = {
-        name: null
-    };
-    $scope.save = function() {
-        console.log($scope.model);
-        console.log($scope.model.name);
-    };
-});
-app.controller('page2Ctrl', function($http, $rootScope) {});
-app.controller('page3Ctrl', function($http, $rootScope) {});
-app.controller('page4Ctrl', function($http, $rootScope) {});
+app.controller('page1Ctrl', function($scope, $http, $rootScope) {});
+app.controller('page2Ctrl', function($scope, $http, $rootScope) {});
+app.controller('page3Ctrl', function($scope, $http, $rootScope) {});
+app.controller('page4Ctrl', function($scope, $http, $rootScope) {});
